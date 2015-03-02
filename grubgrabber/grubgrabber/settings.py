66ec97ee754f9914e6d45d4aa8919a1da6650c96
Isaac Jordan <sheepses6@gmail.com>
2015-03-02 14:05:27 +0000

    Added basic template for base. Added normalize import in scss.

--- grubgrabber/grubgrabber/settings.py
+++ grubgrabber/grubgrabber/settings.py
@@ -82,4 +82,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-TEMPLATE_DIRS = (os.path.join(BASE_DIR, "templates"),)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, "templates"),
+)
