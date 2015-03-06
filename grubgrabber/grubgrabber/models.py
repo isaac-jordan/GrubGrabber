238714fa9289e1e52229ecd6a9e0477c0d3cd0f9
2072704N <2072704N@BO706-18.ad.dcs.gla.ac.uk>
2015-03-06 16:09:10 +0000

    Started models (might want to check them?)

---
+++ grubgrabber/grubgrabber/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.user.user_id
+
+class Favourites(models.Model):
+    user = models.ForeignKey(User)
+    place_id = modeles.CharField(max_legnth = 100)
+    def __unicode__(self):
+        return self.Favourites.Place_ID
+
+class Likes(models.Model):
+    user = models.ForeignKey(User)
+    place_id = modeles.CharField(max_legnth = 100)
+    def __unicode__(self):
+        return self.Likes.Place_ID
+
+class Dislikes(models.Model):
+    user = models.ForeignKey(User)
+    place_id = modeles.CharField(max_legnth = 100)
+    def __unicode__(self):
+        return self.Dislikes.Place_ID
+
+class Blacklist(models.Model):
+    user = models.ForeignKey(User)
+    place_id = modeles.CharField(max_legnth = 100)
+    
+    def __unicode__(self):
+        return self.Blacklist.Place_ID
