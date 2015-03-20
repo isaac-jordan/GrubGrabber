9bcc2c7e288c4e662cf329a085f5f5873049a8f8
2062685c <2062685c@student.gla.ac.uk>
2015-03-20 17:25:18 +0000

    le profile works ken

--- grubgrabber/grubgrabber/urls.py
+++ grubgrabber/grubgrabber/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from registration.backends.simple.views import RegistrationView
 from django.contrib.auth import views
+from django.conf import settings
 
 class MyRegistrationView(RegistrationView):
     def get_success_url(self, request, user):
@@ -16,10 +17,17 @@ urlpatterns = patterns('',
     url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     (r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^add_profile/$', 'grubgrabber.views.register_profile',name='register_profile'),
-    url(r'^profile$', 'grubgrabber.views.profile',name='profile'),
+    url(r'^profile/$', 'grubgrabber.views.profile',name='profile'),
     url(r'^add_favourite/$', 'grubgrabber.views.addFavourite', name='addFavourite'),
     url(r'^add_blacklist/$', 'grubgrabber.views.addBlacklist', name='addBlacklist'),
     url(r'^add_like/$', 'grubgrabber.views.addLike', name='addLike'),
     url(r'^sort_results/$', 'grubgrabber.views.sort_search_results', name='sortResults'),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'^media/(?P<path>.*)',
+        'serve',
+        {'document_root': settings.MEDIA_ROOT}), )
