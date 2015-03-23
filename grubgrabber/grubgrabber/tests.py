381997a910cb330e0456396eed522f6ce4e688af
Isaac Jordan <sheepses6@gmail.com>
2015-03-23 15:09:43 +0000

    Started some tests.

--- grubgrabber/grubgrabber/tests.py
+++ grubgrabber/grubgrabber/tests.py
@@ -1,51 +1,45 @@
 from django.test import TestCase, Client
 from django.core.urlresolvers import reverse
-from grubgrabber.models import Like
+from grubgrabber.models import Like, UserProfile
 from django.contrib.auth.models import User
-from django.test.utils import setup_test_environment
-
-setup_test_environment()
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grubgrabber.settings')
 
 def add_like(place_id, name):
     like = Like.objects.get_or_create(name=name, place_id=place_id)[0]
-    like.save()
     return like
-    
+
 def add_user_and_profile(name, email, password, about):
     u = User.objects.get_or_create(username = name, email=email, password=password)[0]
-    u.save()
     UserProfile.objects.get_or_create(user = u, about = about)
     return u
-    
+
 
 class IndexViewTests(TestCase):
 
     def test_index_view_with_no_recent_eats(self):
-        response = self.client.get(reverse('index'))
+        c = Client()
+        response = c.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No recent eats")
-		
+
     def test_index_view_with_recent_eats(self):
-    
         add_like("ChIJ-4qF7s5FiEgR6bRXbXOZeek", 'Greggs')
-        add_like("ChIJ-4qF7s5FiEgR6bRXbXOZeek", 'Bills')
-        add_like("ChIJ-4qF7s5FiEgR6bRXbXOZeek", 'Stevens')
-        add_like("ChIJ-4qF7s5FiEgR6bRXbXOZeek", 'Bens')
-        
-        response = self.client.get(reverse('index'))
-        
+        add_like("ChIJ-4qF7s5FiEgR6bRXbXOZee", 'Bills')
+        add_like("ChIJ-4qF7s5FiEgR6bRXbXOZek", 'Stevens')
+        add_like("ChIJ-4qF7s5FiEgR6bRXbXOeek", 'Bens')
+
+        c= Client()
+        response = c.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response.context['likes'], 'Greggs')
-        
         no_recent_eats = len(response.context['likes'])
         self.assertEqual(no_recent_eats , 4)
-        
+        self.assertTrue('Greggs' in response.context['likes']) #Fails
+
 class ProfileViewTests(TestCase):
-    
+
     def test_username_shows(self):
-    
         add_user_and_profile("stevo", "stevo@gmail.com", "Greggs", "howdy")
-        
-        response = self.client.get(reverse('index'))
-        
+        c = Client()
+        response = c.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
